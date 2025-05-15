4857cddb6c928cbe463d0754aebcad1204f23e00
Lin Wenhao <linwenhao5@gmail.com>
2025-05-15 21:57:22 +0200

    feat: remove media library component and integrate media display directly in the view

--- resources/views/admin/media.blade.php
+++ resources/views/admin/media.blade.php
@@ -14,8 +14,28 @@
 
     <hr>
     
-    <media-library></media-library>
+    <div class="row">
+        @foreach($media as $item)
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <img src="{{ asset('storage/' . $item->path) }}" class="card-img-top" style="height: 180px; object-fit: cover;">
+                    <div class="card-body text-center">
+                        <p class="card-text">{{ $item->name }}</p>
+                        <delete-confirm
+                        action="{{ route('api.media.delete', $item) }}"
+                        title="Delete file?"
+                        text="Are you sure you want to delete the file '{{ $item->name }}'?"
+                        confirm-button-text="Yes, delete it!"
+                        success-message="File deleted successfully!"
+                        error-message="Failed to delete the file."
+                    >
+                    </delete-confirm>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
 
-@vite('resources/js/media-library.js')
+@vite('resources/js/delete-confirm.js')
