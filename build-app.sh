1866e49249b606b0f8b7e4a73bf5ea02686b32d3
Lin Wenhao <linwenhao5@gmail.com>
2025-06-04 00:40:35 +0200

    refactor: removing redundant cache commands

--- build-app.sh
+++ build-app.sh
@@ -9,9 +9,3 @@ npm run build
 
 # Clear cache
 php artisan optimize:clear
-
-# Cache the various components of the Laravel application
-php artisan config:cache
-php artisan event:cache
-php artisan route:cache
-php artisan view:cache
