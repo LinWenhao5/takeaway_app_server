55b7ff35b2b1804535e446fb3472d475fae22903
Lin Wenhao <linwenhao5@gmail.com>
2025-06-20 18:31:10 +0200

    fix: add Access-Control-Allow-Methods header in index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin: *");
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header('Access-Control-Allow-Headers: Authorization, Content-Type');
 
 define('LARAVEL_START', microtime(true));
