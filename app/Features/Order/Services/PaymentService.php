b4bab5b8cfb590c3348ee3fff064f6390a65150d
Lin Wenhao <linwenhao5@gmail.com>
2025-07-22 13:58:36 +0200

    fix(payment): update webhook URL to use route helper for consistency

--- app/Features/Order/Services/PaymentService.php
+++ app/Features/Order/Services/PaymentService.php
@@ -29,8 +29,8 @@ class PaymentService
             ],
             "description" => "Order #{$order->id}",
             "redirectUrl" => $redirectUrl,
-            // "webhookUrl" => route('api.payment.webhook'),
-            "webhookUrl" => " https://2f601399874d.ngrok-free.app/api/orders/payment-webhook",
+            "webhookUrl" => route('api.payment.webhook'),
+            // "webhookUrl" => " https://2f601399874d.ngrok-free.app/api/orders/payment-webhook",
             "method" => \Mollie\Api\Types\PaymentMethod::IDEAL,
             "metadata" => [
                 "order_id" => $order->id,
