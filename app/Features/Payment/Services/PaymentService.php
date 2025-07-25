9d32f36e258ad434873a4bc2f43fb9e235fc76a5
Lin Wenhao <linwenhao5@gmail.com>
2025-07-25 14:44:31 +0200

    fix(payment): update webhook URL

--- app/Features/Payment/Services/PaymentService.php
+++ app/Features/Payment/Services/PaymentService.php
@@ -28,8 +28,8 @@ class PaymentService
             ],
             "description" => "Order #{$order->id}",
             "redirectUrl" => $redirectUrl,
-            // "webhookUrl" => route('api.payment.webhook'),
-            "webhookUrl" => "https://29b1bd9da48c.ngrok-free.app/api/payments/webhook",
+            "webhookUrl" => route('api.payment.webhook'),
+            // "webhookUrl" => "https://29b1bd9da48c.ngrok-free.app/api/payments/webhook",
             "method" => \Mollie\Api\Types\PaymentMethod::IDEAL,
             "metadata" => [
                 "order_id" => $order->id,
