2c5903594c196f7fd4246d22298aee8c0dff692e
Lin Wenhao <linwenhao5@gmail.com>
2026-05-28 23:36:03 +0200

    feat(scripts): add database migration command to startup script

--- start-container.sh
+++ start-container.sh
@@ -4,11 +4,12 @@
 # Exit the script if any command fails
 set -e
 
+# Run database migrations
+php artisan migrate --force
+
 # Run database seeder
 php artisan db:seed --force
 
 # Generate Swagger documentation
 php artisan l5-swagger:generate
-
-echo "Business startup logic finished."
 
