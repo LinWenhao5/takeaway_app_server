40331ed5c1051bdd76374d1ef48c9556d927d926
Lin Wenhao <linwenhao5@gmail.com>
2026-05-29 19:00:47 +0200

    feat: update mail configuration to use Resend service and implement queue support

--- app/Mail/RegistrationInvitationMail.php
+++ app/Mail/RegistrationInvitationMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class RegistrationInvitationMail extends Mailable
+class RegistrationInvitationMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
