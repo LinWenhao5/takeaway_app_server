8017464a372ca06027956b9327645902e38a5615
Lin Wenhao <linwenhao5@gmail.com>
2026-06-15 23:26:06 +0200

    chore(worker): prioritize printing queue in artisan worker

--- run-worker.sh
+++ run-worker.sh
@@ -3,4 +3,4 @@
 
 # This command runs the queue worker. 
 # The timeout controls how long a job may run before the worker kills it.
-php artisan queue:work --tries=1 --timeout=60
+php artisan queue:work --queue=printing,default --tries=1 --timeout=60
